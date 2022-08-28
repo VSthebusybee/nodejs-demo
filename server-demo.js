a8f11a472685150715df7ae40b62c74e666f6815
VSthebusybee <vsharma0566@gmail.com>
2022-08-28 16:08:29 +0530

    Created server using express in server-demo.js

--- server-demo.js
+++ server-demo.js
@@ -1,4 +1,4 @@
-const http=require('http');
+/* const http=require('http');
 
 const server=http.createServer((req,res)=>{
     res.writeHead(200,{'Content-Type':'text/html'});
@@ -6,4 +6,30 @@ const server=http.createServer((req,res)=>{
 });
 
 server.listen(4000);
+ */
 
+const express=require('express');
+const server=express();
+const port=4000;
+
+const movies=[
+    {
+        year:"2002",
+        movie:"Gang of wasseypur",
+        rating:"9.0+"
+    },
+    {
+        year:"2008",
+        movie:"Gang of wasseypur 2",
+        rating:"7.0+"
+    }
+]
+
+server.get('/',(req,res)=>{
+    
+    res.send(movies);
+})
+
+server.listen(port,() => {
+    console.log("Server is listening port no."+ port);
+})
